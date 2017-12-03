3d0677bcfe2d910ec31362b559b59dfbd180fa6c
yuta0801 <yuta20030801@icloud.com>
2017-12-03 23:42:53 +0900

    エラーをJSONで返すように

--- app.js
+++ app.js
@@ -48,7 +48,10 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send({
+    code: err.status,
+    message: err.message
+  });
 });
 
 module.exports = app;
